5bbba09c2358ee4d584cea5c25d254300ac2e475
wuhao <1847389939@qq.com>
2020-09-11 10:15:18 +0800

    IU-2020.2.1 <wuhao@wuhaos-MacBook-Pro.local Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, yiiguxing.translation.xml, shared-indexes.xml, Default.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, javaeeExternalResources.xml, colors.scheme.xml, notifications.xml, path.macros.xml, databaseDrivers.xml, github.xml, GrepConsole.xml, Default.xml, studioFlags.xml, Class.java, jdk.table.xml, IntelliJ IDEA Classic copy.xml, laf.xml, vcs.xml, toStringTemplates.xml, keymap.xml, mybatis.xml, EasyApiSetting.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @version 1.0
+ * @author wuhao
+ * @Description  $
+ * @Email wuhaowuhao55@163.com
+ * @Date ${DATE} ${TIME}
+ **/
+public enum ${NAME} {
+}
